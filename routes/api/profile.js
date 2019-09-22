ecee63bacee88e0e8ea9460f142dd0b50d6cbbea
Jaden Padua <paduajaden@gmail.com>
2019-09-21 18:50:50 -0700

    Create Validation route to check for status and skills

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult} = require('express-validator/check');
 const Profile = require ('../../models/Profile');
 const User = require('../../models/User');
 // @route  GET api/profile/me
@@ -20,5 +21,19 @@ router.get('/me', auth, async(req,res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route  POST api/profile/
+// @desc   Create or update a USer profile
+// @access Private
+router.post('/', [auth, [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+]], async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+  }
+);
 
 module.exports = router;
